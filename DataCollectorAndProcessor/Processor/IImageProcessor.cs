72f3bdcebb10e81cd54c75aa62029589c5ab26bf
Frederik Alexander Hounsvad <frederik@hounsvad.dk>
2021-10-25 15:45:28 +0200

    Updated diagram, Created ImageProcessor Interface method

--- DataCollectorAndProcessor/Processor/IImageProcessor.cs
+++ DataCollectorAndProcessor/Processor/IImageProcessor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,14 @@ using System.Threading.Tasks;
 namespace Processor
 {
     interface IImageProcessor
-    { 
+    {
+        /// <summary>
+        /// Returns NDVI values for all pixels, by their individual pixels.
+        /// </summary>
+        /// <param name="nearInfrared"></param>
+        /// <param name="infrared"></param>
+        /// <param name="imagePolygon"></param>
+        /// <returns><see cref="NDVIPixel"/></returns>
+        public NDVIPixel[] ProcessImageToNdviPixels(Bitmap nearInfrared, Bitmap infrared, List<(int, int)> imagePolygon);
     }
 }
