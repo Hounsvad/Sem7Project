6816533aa7461892670dd5cba529bd6acecb02ee
Frederik Hounsvad <frederik@hounsvad.dk>
2021-11-04 14:51:27 +0100

    Updated pythonSparkImage Processor with input validation

--- PythonProcessor/main.py
+++ PythonProcessor/main.py
@@ -13,23 +13,38 @@ def getSparkSession(config: dict) -> SparkSession:
     spark = SparkSession.builder \
         .appName(config["sparkAppName"]) \
         .config("spark.master", config["sparkMasterURL"]) \
-        .config("spark.executor.memory", config["SparkMaxMemory"]) \
+        .config("spark.executor.memory", config["sparkMaxMemory"]) \
         .config("spark.sql.streaming.checkpointLocation", config["sparkCheckpointLocationURL"]) \
         .getOrCreate()
     return spark
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    if (len(sys.argv) < 1):
-        print("Needs json configuration")
-        exit(-1)
-    args: dict = json.loads(sys.argv[0])
-    # TODO: Insert input validation
-    # if not ("appsetting" in args and "" ):
-    #    args
 
-    config: dict = json.load(open("./appsettings.json"))
+def generateNDVIPixels(redPixelDF: DataFrame, nirPixelDF: DataFrame, xOffset: float, yOffset: float):
+    cond = [redPixelDF.x == nirPixelDF.x,
+            redPixelDF.y == nirPixelDF.y,
+            redPixelDF.height == nirPixelDF.height,
+            redPixelDF.width == nirPixelDF.width, ]
 
+    return redPixelDF.join(nirPixelDF, cond) \
+        .withColumn("ndvi", expr("(nirIntensity-redIntensity)/(nirIntensity+redIntensity)")) \
+        .withColumn("lattitudeTL", expr(f'{args["lat1"]}+({yOffset}*y)')) \
+        .withColumn("lattitudeBR", expr(f'{args["lat1"]}+({yOffset}*y)')) \
+        .withColumn("longtitudeTL", expr(f'{args["long1"]}+({xOffset}*(y+1))')) \
+        .withColumn("longtitudeBR", expr(f'{args["long1"]}+({xOffset}*(y+1))'))
+
+
+def extractToPixels(valueAlias: str, height: int, width: int, dataframe: DataFrame):
+    return dataframe.withColumn("image.data", explode("image.data")) \
+        .withColumn("y", range(0, height)) \
+        .withColumn("image.data", explode("image.data")) \
+        .withColumn("x", range(0, width * height)) \
+        .withColumn("x", expr("x % image.width")) \
+        .withColumn("value", column("image.data")[0]) \
+        .toDF(column("x"), column("y"), column("value").alias(valueAlias), column("image.height").alias("height"),
+              column("image.width").alias("width"))
+
+
+def main(args: dict, config: dict):
     spark = getSparkSession(config)
 
     redImageDF: DataFrame = spark.read.format("image").load(
@@ -43,31 +58,11 @@ if __name__ == '__main__':
     xOffset: int = (args["long2"] - args["long1"]) / width
     yOffset: int = (args["lat2"] - args["lat1"]) / height
 
-    redDF: DataFrame = redImageDF.withColumn("image.data", explode("image.data")) \
-        .withColumn("y", range(0, height)) \
-        .withColumn("image.data", explode("image.data")) \
-        .withColumn("x", range(0, width * height)) \
-        .withColumn("x", expr("x % image.width")) \
-        .withColumn("value", column("image.data")[0]) \
-        .toDF(column("x"), column("y"), column("value").alias("redIntensity"), column("image.height").alias("height"),
-              column("image.width").alias("width"))
+    redDF: DataFrame = extractToPixels("redIntensity", height, width, redImageDF)
 
-    nirDF: DataFrame = nirImageDF.withColumn("image.data", explode("image.data")) \
-        .withColumn("y", range(0, height)) \
-        .withColumn("image.data", explode("image.data")) \
-        .withColumn("x", range(0, width * height)) \
-        .withColumn("x", expr("x % image.width")) \
-        .withColumn("value", column("image.data")[0]) \
-        .toDF(column("x"), column("y"), column("value").alias("nirIntensity"), column("image.height").alias("height"),
-              column("image.width").alias("width"))
+    nirDF: DataFrame = extractToPixels("nirIntensity", height, width, nirImageDF)
 
-    cond = [redDF.x == nirDF.x, redDF.y == nirDF.y, redDF.height == nirDF.height, redDF.width == nirDF.width, ]
-    combinedImageDF: DataFrame = redDF.join(nirDF, cond) \
-        .withColumn("ndvi", expr("(nirIntensity-redIntensity)/(nirIntensity+redIntensity)")) \
-        .withColumn("lattitudeTL", expr(f'{args["lat1"]}+({yOffset}*y)')) \
-        .withColumn("lattitudeBR", expr(f'{args["lat1"]}+({yOffset}*y)')) \
-        .withColumn("longtitudeTL", expr(f'{args["long1"]}+({xOffset}*(y+1))')) \
-        .withColumn("longtitudeBR", expr(f'{args["long1"]}+({xOffset}*(y+1))')) \
+    combinedImageDF: DataFrame = generateNDVIPixels(redDF, nirDF, xOffset, yOffset) \
         .withColumn("combined", array(col("ndvi"), col("lattitudeTL"), col("lattitudeBR"), col("longtitudeTL"),
                                       col("longtitudeBR")))
 
@@ -76,7 +71,55 @@ if __name__ == '__main__':
         .selectExpr("CAST(jsonValue AS STRING)") \
         .writeStream \
         .format("kafka") \
-        .option("kafka.bootstrap.servers", config["kafkaURL"])\
-        .option("topic", config["ndviPixelIngestTopic"]).outputMode("complete")\
-        .start()\
+        .option("kafka.bootstrap.servers", config["kafkaURL"]) \
+        .option("topic", config["ndviPixelIngestTopic"]).outputMode("complete") \
+        .start() \
         .awaitTermination()
+
+
+def validateArgsContents(args: dict):
+    requiredArgsFields: list = ["lat1", "lat2", "long1", "long2", "polygon"]
+    for item in requiredArgsFields:
+        if item not in args:
+            print(f'Item:"{item}", is missing from the args json')
+            exit(2)
+    if len(args["polygon"]) < 3:
+        print('Polygon field in args contained less than 3 items')
+    for item in args["polygon"]:
+        if "lat" not in item:
+            print("lat missing from an entry in args.polygon")
+            exit(4)
+        if "long" not in item:
+            print("long missing from an entry in args.polygon")
+
+
+def validateConfigContents(config: dict):
+    requiredAppsettingsFields: list = ["hdfsImageIngestPath", "hdfsImageIngestRedImage", "hdfsImageIngestNirImage",
+                                       "kafkaURL", "ndviPixelIngestTopic", "sparkAppName", "sparkMasterURL",
+                                       "sparkMaxMemory", "sparkCheckpointLocationURL"]
+    for item in requiredAppsettingsFields:
+        if item not in config:
+            print(f'Item:"{item}", is missing from the appsettings.json file')
+            exit(8)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    # Validate Input
+    if len(sys.argv) < 1:
+        print("Needs json configuration")
+        exit(1)
+
+    # Extract Args As Json
+    args: dict = json.loads(sys.argv[0])
+
+    # Validate Args exists
+    validateArgsContents()
+
+    # Extract config values from appsettings
+    config: dict = json.load(open(args["appsettings"]))
+
+    # Validate Appsettings
+    validateConfigContents(config)
+
+    main(args, config)
